7a81b44e8b6db93f5e84be33a78f4da7316e5eea
nitin-das <nitin.das@thomson.co.uk>
2020-01-17 17:12:13 +0000

    Modified code

---
+++ efs-mount-target.tf
@@ -0,0 +1,7 @@
+resource "aws_efs_mount_target" "efs_mount_target" {
+  count          = length(var.subnet_ids)
+  file_system_id = aws_efs_file_system.efs_file_system.id
+  subnet_id      = var.subnet_ids[count.index]
+  security_groups = var.security_groups
+}
+
