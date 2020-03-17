ff9826f2c02a1bc244057cf30d98d5016335fb4f
nitin-das <nitin.das@thomson.co.uk>
2020-03-17 12:41:49 +0000

    Modified Code

--- efs.tf
+++ efs.tf
@@ -1,16 +1,15 @@
 resource "aws_efs_file_system" "efs_file_system" {
-  creation_token                  = var.creation_token
-  encrypted                       = var.encrypted
-  kms_key_id                      = var.kms_key_id
-  performance_mode                = var.performance_mode
-  provisioned_throughput_in_mibps = var.provisioned_throughput_in_mibps
-  tags                            = var.common_tags
-  throughput_mode                 = var.throughput_mode
-
+  creation_token = var.creation_token
+  encrypted      = var.encrypted
+  kms_key_id     = var.kms_key_id
   dynamic "lifecycle_policy" {
-    for_each = var.lifecycle_policy
+    for_each = length(keys(var.lifecycle_policy)) == 0 ? [] : [var.lifecycle_policy]
     content {
       transition_to_ia = lookup(lifecycle_policy.value, "transition_to_ia", null)
     }
   }
+  performance_mode                = var.performance_mode
+  provisioned_throughput_in_mibps = var.provisioned_throughput_in_mibps
+  tags                            = var.tags
+  throughput_mode                 = var.throughput_mode
 }
